4875d1110eef99375067966c66148ded862289cf
Dustin Heckmann <dustin.heckmann@thoughtworks.com>
2019-08-24 16:12:15 +0200

    Add formatting to deployment script output

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env bash
-
+printf '\n============================================\n'
+printf 'Local images:\n'
 docker-compose images
 
-echo 'Pulling images...'
+printf '\n============================================\n'
+printf '\nPulling images...\n\n'
+
 docker-compose pull
+
+printf '\n============================================\n'
+printf '\nLocal images:\n\n'
+
 docker-compose images
+
+printf '\n============================================\n'
+printf '\nCurrently running containers:\n\n'
 
 docker ps
 
-echo 'Updating containers...'
+printf '\n============================================\n'
+printf '\nUpdating containers...\n\n'
+
 docker-compose up -d --build
+
+printf '\n============================================\n'
+printf '\nCurrently running containers:\n\n'
 
 docker ps
